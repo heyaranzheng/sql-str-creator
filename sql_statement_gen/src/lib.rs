6f527e68390780ecd9636e7022ce532003cfa2a3
Aran Z <heyaranzheng@hotmail.com>
2026-05-24 18:38:44 +0800

    update

---
+++ sql_statement_gen/src/lib.rs
@@ -0,0 +1,3 @@
+//export the trait SqlStatement, and the derive macro SqlStatement
+pub use sql_statement_derive::SqlStatement;
+pub use sql_statement_trait::SqlStatement;
---
+++ sql_statement_gen/tests/export_test.rs
@@ -0,0 +1,18 @@
+use sql_statement_gen::SqlStatement;
+
+
+#[derive(SqlStatement)]
+struct MyStruct {
+    field1: i32,
+    field2: String,
+}
+
+#[test]
+fn exprot_text() {
+    let test = MyStruct {
+        field1: 1,
+        field2: "test".to_string(),
+    };
+    let sql = test.create_table_sql();
+    println!("SQL: {}", sql);   
+}
